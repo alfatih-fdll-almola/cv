4a2d171ce61c531c77670191ebf3e2ca5aa95969
alfatih221 <74567396+alfatih221@users.noreply.github.com>
2020-11-19 23:56:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta name="viewpor" content=" width=device-width, initial-scale=1.8">
+        <title>ALFATiH Fdll ALMOLA</title>
+    </head>
+<body bgcolor="#ffffff">
+    <center>
+    <h1  <font color="b">in tha name of allah<h1/>
+    <h1   <font color="b">Emirates college of science & technology<h1/>
+    <h3  <font color="b"> Department of information technology<h3/>
+    <h4  <font color="b"> ALFATiH Fdll ALMOLA<h4/>
+    <h4  <font color="b">  level : thard<h4/>
+    </center>
+    
+              
+<?php
+?>
+
+       <br>
+        
+</body>
+</html>
